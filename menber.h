21c6d0c7b0d9d92259cc55c5d2716af321c4a6fd
KM <KM>
2020-12-13 15:10:15 +0800

    asdf

--- menber.h
+++ menber.h
@@ -1,7 +1,7 @@
 /*
 会员头文件
 需要实现：
-1.定义会员结构体,包含会员姓名，性别，住所，位置等信息
+1.定义会员结构体,包含会员姓名，住所,已占用设施等信息
 2.用链表/数组存储会员结构体信息
 3.写函数实现会员的添加删除修改
 4.写函数，让会员能买卖房屋，占用/解除占用设施，选择服务人员,入住房屋等；
@@ -11,3 +11,24 @@
 #include"house.h"
 #include"service.h"
 #include"ui.hpp"
+
+using namespace std;
+class menber
+{
+private:
+    string name;
+    vector<House> estates;
+            
+    /* data */
+public:
+    menber(/* args */);
+    ~menber();
+};
+
+menber::menber(/* args */)
+{
+}
+
+menber::~menber()
+{
+}
