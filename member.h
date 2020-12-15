d39c53aec644f6fab2ba557e016f7f837c76b8dd
KM <KM>
2020-12-15 14:32:55 +0800

    12

--- member.h
+++ member.h
@@ -52,7 +52,7 @@ int Member::Access_facility(Facility*head)
         {
             current->istaken = 1;
             current->occupier = name;
-            facilitytaken = current->id;
+            facilitytaken = *curren
             break;
         }
     }
